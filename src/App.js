e844ef3b730d0d1de816f8e0c6f78696e48b95ae
Sayak Maity <smaity@college.harvard.edu>
2023-04-01 22:30:57 -0400

    large stats

--- src/App.js
+++ src/App.js
@@ -47,6 +47,8 @@ const Container = styled.div`
 
 export default function Home() {
   const [transactions, setTransactions] = useState([]);
+  const [totalEnergy, setTotalEnergy] = useState(0);
+
   const largeStat = 12345;
   const smallStats = [
     { label: "Stat 1", value: 234 },
@@ -55,30 +57,49 @@ export default function Home() {
   ];
   const ETHERSCAN_API_KEY = "247XENS1UNZU23RTB3WB2Y4ZDHWV58MH1N";
 
+  function calculateEnergyForPastMonth(transactions) {
+    const oneMonthAgo = new Date();
+    oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+
+    return transactions.reduce((acc, tx) => {
+      if (new Date(tx.timestamp) > oneMonthAgo) {
+        return acc + parseFloat(tx.energy);
+      }
+      return acc;
+    }, 0);
+  }
+
   async function fetchTransactionData(address) {
     const url = `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=desc&apikey=${ETHERSCAN_API_KEY}`;
     const getTransactionType = (input) => {
-      if (input === '0x') {
-        return 'Normal';
-      } else if (input.startsWith('0x')) {
-        return 'Smart Contract';
+      if (input === "0x") {
+        return "Normal";
+      } else if (input.startsWith("0x")) {
+        return "Smart Contract";
       } else {
-        return 'Internal';
+        return "Internal";
       }
     };
+
     try {
       const response = await fetch(url);
       const data = await response.json();
       if (data.status === "1") {
-        // Process and return the transaction data as needed
-        return data.result.map((tx) => ({
-          timestamp: (new Date(tx.timeStamp * 1000)).toDateString(),
+        // Process the transaction data as needed
+        const processedTransactions = data.result.map((tx) => ({
+          timestamp: new Date(tx.timeStamp * 1000).toDateString(),
           type: getTransactionType(tx.input),
           gasUsed: tx.gasUsed,
-          percent: parseFloat(tx.gasUsed / tx.gas * 100).toFixed(2),
-          energy: parseFloat(tx.gasUsed * tx.gasPrice / 10**15).toFixed(2),
+          percent: parseFloat((tx.gasUsed / tx.gas) * 100).toFixed(2),
+          energy: parseFloat((tx.gasUsed * tx.gasPrice) / 10 ** 15).toFixed(2),
           success: tx.isError === "0",
         }));
+
+        // Calculate the total energy consumption for the past month
+        const totalEnergy = calculateEnergyForPastMonth(processedTransactions);
+        setTotalEnergy(totalEnergy);
+
+        return processedTransactions;
       } else {
         console.error("Error fetching transactions:", data.message);
         return [];
@@ -96,7 +117,7 @@ export default function Home() {
           const provider = new ethers.providers.Web3Provider(window.ethereum);
           const signer = provider.getSigner();
           // const address = await signer.getAddress();
-          const address = '0x97c46EeC87a51320c05291286f36689967834854'
+          const address = "0x97c46EeC87a51320c05291286f36689967834854";
 
           // Replace the following line with the API call to fetch transactions
           const data = await fetchTransactionData(address);
@@ -127,10 +148,8 @@ export default function Home() {
       </Navbar>
       <div className="container">
         <main className="main">
-          {/* <h1 className="title">Welcome to PassTheGas!</h1> */}
-          {/* Add this block for displaying the statistics */}
           <StatsContainer>
-            <LargeStat>{largeStat.toLocaleString()}</LargeStat>
+            <LargeStat>{totalEnergy.toFixed(2).toLocaleString()} kWh</LargeStat>
             <SmallStatsContainer>
               {smallStats.map((stat, index) => (
                 <SmallStat key={index}>
